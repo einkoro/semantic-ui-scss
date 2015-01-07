3a1d11428adaf830b5bfb6c69cd36f8469a3213b
Einkoro <jpingel@bitpiston.com>
2015-01-07 14:01:49 -0800

    Enhanced convert and implemented basic concat task

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,18 @@ var gulp        = require('gulp-param')(require('gulp'), process.argv),
     console     = require('better-console'),
     del         = require('del'),
     inquirer    = require('inquirer'),
-    fs          = require('fs'),
+    //fs          = require('fs'),
     githubApi   = require('github'),
+    glob        = require('glob'),
+    path        = require('path'),
     runSequence = require('run-sequence'),
+    streamQueue = require('streamqueue'),
 
     // gulp plugins
     clone       = require('gulp-clone'),
     concat      = require('gulp-concat'),
     copy        = require('gulp-copy'),
+    foreach     = require('gulp-foreach'),
     git         = require('gulp-git'),
     print       = require('gulp-print'),
     prompt      = require('gulp-prompt'),
@@ -29,13 +33,30 @@ var gulp        = require('gulp-param')(require('gulp'), process.argv),
  * Settings
  */
 
-var tmpPath       = 'tmp',
-    buildPath     = 'build',
-    semanticOwner = 'Semantic-Org',
-    semanticRepo  = 'Semantic-UI',
-    semanticUrl   = 'https://github.com/' + semanticOwner + '/' + semanticRepo,
-    semanticPath  = 'tmp/' + semanticRepo,
-    semanticTag   = undefined;
+var semantic = {
+        owner: 'Semantic-Org',
+        repo:  'Semantic-UI',
+        url:   'https://github.com/Semantic-Org/Semantic-UI',
+        path:  'tmp/Semantic-UI',
+        tag:   undefined,
+    },
+    paths    = {
+        tmp:  {
+            root:        'tmp',
+            definitions: 'tmp/definitions',
+            themes:      'tmp/themes'
+        },
+        src:  {
+            root:        semantic.path + '/src',
+            definitions: semantic.path + '/src/definitions',
+            themes:      semantic.path + '/src/themes'
+        },
+        dest: {
+            root: 'dist',
+            scss: 'dist/scss',
+            js:   'dist/js'
+        }
+    };
 
 
 /*
@@ -46,15 +67,16 @@ var tmpPath       = 'tmp',
 gulp.task('default', function() {
     runSequence([
         'clean',
-        'fetch'
+        'fetch',
+        'build'
     ]);
 });
 
 // Clean the temporary and build directories
 gulp.task('clean', function() {
-    console.log('Cleaning temporary and build directories...');
+    console.log('Cleaning temporary directory...');
 
-    del([tmpPath + '/*', buildPath + '/*']);
+    del([tmp.root + '/*']);
 
     console.log('Done cleaning directories');
 });
@@ -73,11 +95,10 @@ gulp.task('fetch', function(tag) {
     }
 
     // Determine the latest release by tag
-    function determineLatestTag()
-    {
+    function determineLatestTag() {
         github.releases.listReleases({
-            owner: semanticOwner,
-            repo: semanticRepo,
+            owner:    semantic.owner,
+            repo:     semantic.repo,
             per_page: 1
         }, function(error, result) {
             promptForTag(result[0].tag_name);
@@ -85,12 +106,11 @@ gulp.task('fetch', function(tag) {
     }
 
     // Prompt for the tag to fetch defaulting to the latest
-    function promptForTag(latestTag)
-    {
+    function promptForTag(latestTag) {
         var questions = [
             {
-                type: 'input',
-                name: 'tag',
+                type:    'input',
+                name:    'tag',
                 message: 'Which release would you like to fetch?',
                 default: latestTag
             }
@@ -102,27 +122,25 @@ gulp.task('fetch', function(tag) {
     }
 
     // Clone Semantic UI repository
-    function cloneRepo(desiredTag)
-    {
-        git.clone(semanticUrl, {
+    function cloneRepo(desiredTag) {
+        git.clone(semantic.url, {
             args: '-q',
-            cwd: tmpPath
-        }, function (error) {
+            cwd:  paths.tmp.root
+        }, function(error) {
             if (error) throw error;
 
-            semanticTag = desiredTag;
+            semantic.tag = desiredTag;
 
             checkoutTag(desiredTag);
         });
     }
 
     // Checkout desired Semantic UI release
-    function checkoutTag(desiredTag)
-    {
+    function checkoutTag(desiredTag) {
         git.checkout('tags/' + desiredTag, {
             args: '-q',
-            cwd: semanticPath
-        }, function (error) {
+            cwd:  semantic.path
+        }, function(error) {
             if (error) throw error;
 
             console.log('Done fetching ' + desiredTag);
@@ -132,23 +150,111 @@ gulp.task('fetch', function(tag) {
 
 // Build SCSS version of Semantic UI
 gulp.task('build', function() {
+    runSequence([
+        'convert',
+        'concat'
+    ]);
+});
 
-    var semanticSrcPath = semanticPath + '/src';
+// Convert LESS to SCSS
+gulp.task('convert', function() {
+    console.log('Converting LESS to SCSS');
 
-    lessToScss();
+    var sources = [
+        paths.src.definitions + '/*/*.less',
+        paths.src.themes + '/*/*/*.variables',
+        paths.src.themes + '/*/*/*.overrides'
+    ];
 
-    // Convert LESS files to SCSS
-    function lessToScss()
-    {
-        console.log('Converting LESS to SCSS');
-        
-        return gulp.src([semanticSrcPath + '/definitions/*/*.less'])
-            .pipe(replace(/@(?!font-face|import|media|keyframes|-)/g, '$'))
-            .pipe(replace(/\.([\w\-]*)\s*\((.*)\)\s*\{/g, '@mixin \\1\(\\2\)\n{'))
-            .pipe(replace(/\.([\w\-]*\(.*\)\s*;)/g, '@include \1'))
-            .pipe(replace(/~"(.*)"/g, '#{"\1"}'))
-            .pipe(replace(/spin\(/g, 'adjust-hue('))
-            .pipe(rename({extname: '.scss'}))
-            .pipe(gulp.dest(buildPath + '/scss'));
+    return gulp.src(sources, { base: process.cwd() })
+
+        // Remove @import '../../theme.config'; from definitions
+        .pipe(replace(/\.loadUIOverrides\(\);/g, ''))
+
+        // Remove .loadUIOverrides(); from definitions
+        .pipe(replace(/@import '\.\.\/\.\.\/theme\.config';/g, ''))
+
+        // Replace variables (@ with $) and prefix with the filename
+        // uppercase first letter of the match to maintain camelcase
+        .pipe(foreach(function(stream, file) {
+            var basename = path.basename(file.relative, path.extname(file.relative));
+
+            return stream.pipe(replace(/@(?!font-face|import|media|keyframes|-)(.{1})/g, function(string, firstLetter) {
+                if (basename == 'site') {
+                    var replacement = '$' + firstLetter;
+                }
+                else {
+                    var replacement = '$' + basename + firstLetter.toUpperCase();
+                }
+                return replacement;
+            }));
+        }))
+
+        // Replace mixins
+        .pipe(replace(/\.([\w\-]*)\s*\((.*)\)\s*\{/g, '@mixin $1\($2\)\n{'))
+
+        // Replace includes
+        .pipe(replace(/\.([\w\-]*\(.*\)\s*;)/g, '@include $1'))
+
+        // Replace string literals
+        .pipe(replace(/~"(.*)"/g, '#{"$1"}'))
+
+        // Replace spin with adjust-hue
+        .pipe(replace(/spin\(/g, 'adjust-hue('))
+
+        // Rename less files to scss
+        .pipe(rename(function(path) {
+            path.dirname = path.dirname.replace(new RegExp(paths.src.root + '/', 'g'), '');
+
+            if (path.extname == '.less') {
+                path.extname = '.scss';
+            }
+        }))
+
+        .pipe(gulp.dest(paths.tmp.root));
+
+    // Concat theme overrides into definitions
+    function concatOverrides() {
+
     }
+
+    // Fix assset paths
+    function fixAssets() {
+
+    }
+});
+
+// Concat theme variables into one settings file per theme
+gulp.task('concat', function() {
+    console.log('Concatenating variables into settings files');
+
+    var themes = glob.sync(paths.tmp.themes + '/*/'),
+        stream = new streamQueue({ objectMode: true });
+
+    themes.forEach(function(themeDir) {
+        var themeName = themeDir.match(/.+\/(.+)\/$/)[1];
+
+        console.log(themeDir);
+        console.log(themeDir + '*/*.variables');
+        console.log(paths.dest.scss + '/themes/' + themeName + '/');
+
+        stream.queue(
+            gulp.src(themeDir + '*/*.variables')
+
+            // Comment out variables
+            //.pipe(replace(/^\$(?!font-face|import|media|keyframes|-)/g, '// $'))
+
+            // Add new lines before opening comments
+            .pipe(replace(/\/(\*{2,})/g, '\n\n/$1'))
+
+            // Concat the variables as a partial
+            .pipe(concat('_variables.scss'))
+
+            // Organize by theme name
+            .pipe(gulp.dest(paths.dest.scss + '/themes/' + themeName))
+        );
+
+    });
+
+    return stream;
 });
